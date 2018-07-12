dd639dff807fa47f4684c2e0e6cb1fd0bd134b5f
Bhavadharani98 <41136666+Bhavadharani98@users.noreply.github.com>
2018-07-12 10:23:55 +0530

    Create POSNEG.java

---
+++ POSNEG.java
@@ -0,0 +1,17 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int number=98;
+		if(number>=1 ){
+			System.out.println("Positive");
+		}
+		else if(number <1){
+			System.out.println("Negative");
+		}
+		else{
+			System.out.println("Zero");
+		}
+	}
+}
+		
