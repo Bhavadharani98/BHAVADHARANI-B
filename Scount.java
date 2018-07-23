d7333445efe07c89f97aec0584232eae07855da0
Bhavadharani98 <41136666+Bhavadharani98@users.noreply.github.com>
2018-07-23 17:28:16 +0530

    Update Scount.java

--- Scount.java
+++ Scount.java
@@ -4,6 +4,11 @@ public static void main(String[] args){
 	Scanner v=new Scanner(System.in);
 	String number_of_words=v.nextLine();
 	String []a=number_of_words.split(" ");
+	if(a.length==0){
+		System.out.println("1");
+	}
+	else if(a.length>=1 && a.length<=1000){
 	System.out.println(a.length-1);
+}
 }
 }
