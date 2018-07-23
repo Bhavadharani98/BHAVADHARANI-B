829a33c5065cc88ba7b4591aa4adb4f9aef66fa2
Bhavadharani98 <41136666+Bhavadharani98@users.noreply.github.com>
2018-07-23 17:20:17 +0530

    Create Scount.java

---
+++ Scount.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Scount {
+public static void main(String[] args){
+	Scanner v=new Scanner(System.in);
+	String number_of_words=v.nextLine();
+	String []a=number_of_words.split(" ");
+	System.out.println(a.length-1);
+}
+}
