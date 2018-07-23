0b612e5dfaad2c8506c209d1b0ef490756372243
Bhavadharani98 <41136666+Bhavadharani98@users.noreply.github.com>
2018-07-23 14:13:14 +0530

    Update Srev.java

--- Srev.java
+++ Srev.java
@@ -4,8 +4,10 @@ public static void main(String []args){
 	Scanner v=new Scanner(System.in);
 	String string =v.nextLine();
 	String g="";
+	if(string.length()>=1 && string.length()<=100000){
 	for(int i=string.length()-1;i>=0;i--){
 		g=g+string.charAt(i);
+	}
 	}
 	String m=g.replaceAll("[aeiou]","");
 	System.out.println(m);
